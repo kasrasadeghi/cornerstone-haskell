3ff57b3f193ef56b0cb113ccdedaa46752efba64
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-05-13 20:50:04 -0500

    more work on pass. need to test blockify

--- src/Pass.hs
+++ src/Pass.hs
@@ -1,15 +1,30 @@
 module Pass where
 
 import Parse
-    
-blockifyDef (Texp value tls) =
-    Texp value $ (flip map) tls (\tl -> case tl of
-                                          (Texp "def" (a:b:c:rest)) -> (Texp "def" (a:b:c:[(Texp "do" rest)]))
-                                          _ -> tl)
-                                                
--- blockifyTL tl = case tl of
---                  (Texp "def" (a:b:c:rest)) -> (Texp "def" (a:b:c:[(Texp "do" rest)]))
---                  _ -> tl
 
+blockify = blockifyDef . blockifyIf
+
+-- prog
+blockifyDef (Texp value tls) =
+    Texp value $ (flip map) tls blockifyDefTL
+
+-- toplevel
+blockifyDefTL tl = case tl of
+          {- def -}  Texp "def" (a:b:c:rest) -> Texp "def" (a:b:c:[(Texp "do" rest)])
+                     _ -> tl
+
+-- prog
 blockifyIf (Texp value tls) =
-    Texp value $ (flip map) tls
+    Texp value $ (flip map) tls blockifyIfTL
+
+-- toplevel
+blockifyIfTL tl = case tl of
+         {- def -}  Texp "def" (a:b:c:rest) -> Texp "def" (a:b:c:(map blockifyIfStmt rest))
+                    _ -> tl
+
+-- stmt
+blockifyIfStmt stmt = case stmt of
+         {- if-stmt -}  (Texp "if" (cond:stmts)) -> (Texp "if" (cond:[Texp "do" (map blockifyIfStmt stmts)]))
+         {- do-stmt -}  (Texp "do" stmts) -> (Texp "do" (map blockifyIfStmt stmts))
+                        _ -> stmt
+                                                       
